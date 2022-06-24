915e7d548a9d6ca533ce4561010fdc5ee569b6be
Blue Lynxes <blue.lynxes@gmail.com>
2022-06-25 01:20:27 +0200

    Added about Window

--- source/gui/application.cpp
+++ source/gui/application.cpp
@@ -16,12 +16,26 @@ namespace hn
 			this->application->run(*this->insertData_Window);
 		}
 
+		void Application::initAboutWindow()
+		{
+			about_Builder = Gtk::Builder::create_from_file("../ui/HentaiNeko.glade");
+			about_Builder->get_widget<Gtk::Window>("about_window", about_Window);
+		}
+
+		void Application::showAboutWindow()
+		{
+			about_Window->show_all();
+		}
+
 		void Application::initInsertDataWindow()
 		{
 			insertData_Builder = Gtk::Builder::create_from_file("../ui/HentaiNeko.glade");
 			insertData_Builder->get_widget<Gtk::Window>("main_window", insertData_Window);
 			insertData_Builder->get_widget<Gtk::HeaderBar>("header", insertData_HeaderBar);
 			insertData_Window->set_titlebar(*insertData_HeaderBar);
+
+			insertData_Builder->get_widget<Gtk::Button>("info_button", insertData_Button_ShowAboutWindow);
+			insertData_Button_ShowAboutWindow->signal_clicked().connect(sigc::mem_fun(*this, &Application::showAboutWindow));
 		}
 
 		void Application::showInsertDataWindow()
--- source/gui/application.hpp
+++ source/gui/application.hpp
@@ -10,6 +10,9 @@ namespace hn
 		public:
 			Application();
 			void run();
+			// About Window
+			void initAboutWindow();
+			void showAboutWindow();
 			// Insert Data Window
 			void initInsertDataWindow();
 			void showInsertDataWindow();
@@ -18,9 +21,17 @@ namespace hn
 
 			// For the moment the Main Window, in the future
 			// the main Window will be a greeter/collection display
+			// NOTE: I'm making one builder per component, since in the
+			// future those component will each have it's own UI file.
+			// For the moment those are useless/wasteful.
 			Glib::RefPtr<Gtk::Builder> insertData_Builder;
 			Gtk::Window* insertData_Window;
 			Gtk::HeaderBar* insertData_HeaderBar;
+			Gtk::Button* insertData_Button_ShowAboutWindow;
+
+			Glib::RefPtr<Gtk::Builder> about_Builder;
+			Gtk::Window* about_Window;
+
 		};
 	}
 }
